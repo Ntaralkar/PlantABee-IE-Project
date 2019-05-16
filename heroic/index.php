7c9aba330b74e941e6b2b1ec76799f655e60b1f2
Ntaralkar <41529589+Ntaralkar@users.noreply.github.com>
2019-05-16 11:39:57 +1000

    Add files via upload

---
+++ heroic/index.php
@@ -0,0 +1,440 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Heroic Features - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/heroic-features.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+	  <!-- Navigation 
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        //<ul class="navbar-nav ml-auto">
+        //  <li class="nav-item active">
+        //    <a class="nav-link" href="#">Home
+        //      <span class="sr-only">(current)</span>
+       //     </a>
+       //   </li>
+         // <li class="nav-item">
+        //    <a class="nav-link" href="#">About</a>
+        //  </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>-->
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4">
+      <h1 class="display-3">A Warm Welcome!</h1>
+      <p class="lead">Plant Display Information.</p>
+      <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
+    </header>
+	<div class="card-info">
+		<p> Test display</p>
+	</div>
+    <!-- Page Features -->
+    <div class="row text-center">
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/08/28/23/24/sunflower-1627193_960_720.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Sunflower</h4>
+            <p class="card-text">Helianthus annuus</p>
+          </div
+
+          <div class="card-footer">
+            <a href="card-info" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="Abelia_X_grandiflora.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Glossy abelia</h4>
+            <p class="card-text">Abelia grandiflora.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="Acacia_baileyana.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Cootamundra wattle</h4>
+            <p class="card-text">Acacia baileyana</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+	  
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="currawang.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Currawang</h4>
+            <p class="card-text">Acacia doratoxylon</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+	  
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="nativehibiscus.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Native hibiscus</h4>
+            <p class="card-text">Alyogyne huegelii</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+	  
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="smoothbarkedapple.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Smooth-barked Apple</h4>
+            <p class="card-text">Angophora costata</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+	  
+	  
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="hairpinbanksia.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Hairpin banksia</h4>
+            <p class="card-text">Banksia spinulosa</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+	  
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="coastbanksia.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Coast Banksia</h4>
+            <p class="card-text">Banksia Integrifolia</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+	  
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="borage.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Borage</h4>
+            <p class="card-text">Borago officinalis</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+	  
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="sawtoothbanksia.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Saw-tooth banksia</h4>
+            <p class="card-text">Banksia serrata</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+	
+	      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="redwillowbottlebrush.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Red Willow Bottlebrush</h4>
+            <p class="card-text">Callistemon salignus</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="dwarfbottlebrush.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Dwarf bottlebrush</h4>
+            <p class="card-text">Callistemon subulatus</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	  
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="weepingbottlebrush.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Weeping Bottlebrush</h4>
+            <p class="card-text">Callistemon Viminalis</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	  
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="bluishpigface.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Bluish Pigface</h4>
+            <p class="card-text">Carpobrotus Glaucescens</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="yellowbloodwood.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Yellow bloodwood</h4>
+            <p class="card-text">Corymbia Eximia</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="redfloweringgum.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Red Flowering Gum</h4>
+            <p class="card-text">Corymbia Ficifolia</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="spottedgum.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Spotted Gum</h4>
+            <p class="card-text">Corymbia Maculata</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="ellangowan.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Ellangowan Poion Bush</h4>
+            <p class="card-text">Eremophila Deserti</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="sugargum.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Sugar Gum</h4>
+            <p class="card-text">Eucalyptus Cladocalyx</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="stoneymallee.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Stoney Mallee</h4>
+            <p class="card-text">Eucalyptus Diversifolia</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="Eucalyptus erythrocorys.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Red Cap Gum</h4>
+            <p class="card-text">Eucalyptus Erythrocorys</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="yellowmallee.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Yellow Mallee</h4>
+            <p class="card-text">Eucalyptus Incrassata</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="crimsonmallee.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Crimson Mallee</h4>
+            <p class="card-text">Eucalyptus Lansdowneana</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	        <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="largefruitedyellowgum.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Large-Fruited Yellow Gum</h4>
+            <p class="card-text">Eucalyptus Leucoxylon</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	  <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="greybox.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Grey Box</h4>
+            <p class="card-text">Eucalyptus Microcarpa</p>
+			<button class="tablink" onclick="openCity('Oslo', this, 'orange')">Oslo</button>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+
+	
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Plant-A-Bee 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  
+  <script>
+function openCity(cityName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(cityName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+</script>
+  
+</body>
+
+</html>
